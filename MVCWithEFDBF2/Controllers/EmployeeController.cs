6b4eecae2e8664e362a6e8096518d18e4a26180e
sonika1311 <sonikakumaribom@gmail.com>
2024-08-02 03:12:30 +0530

    Display all employees

---
+++ MVCWithEFDBF2/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCWithEFDBF2.Models;
+
+namespace MVCWithEFDBF2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        MVCDBEntities dc = new MVCDBEntities();
+        public ViewResult DisplayEmployees()
+        {
+            var Emps = dc.Employees;
+                //.Where(E =>E.Status==true);
+            return View(Emps);
+        }
+    }
+}
